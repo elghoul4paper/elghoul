75da7054d053592d39e03f3f00a49432c45e31a9
elghoul4paper <elghoul4paper@gmail.com>
2025-03-30 01:30:29 +0200

    v1

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/logo/",
+  base: "/elghoul/",
   // server: {
   //   headers: {
   //     "Cache-Control": "max-age=31536000, immutable",
